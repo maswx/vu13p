4668d6330c73bd4345f88a72fefc96b14d16b1e0
masw <masw@masw.tech>
2023-03-27 23:26:37 +0800

    add xdc files

---
+++ prj/07_pcie_ddr4_dma/extn/vivado/xdc.tcl
@@ -0,0 +1,10 @@
+import_files {
+    ../../../../xdc/ddr4_72/ddr4_c0.xdc
+    ../../../../xdc/ddr4_72/ddr4_c1.xdc
+    ../../../../xdc/ddr4_72/ddr4_c2.xdc
+    ../../../../xdc/ddr4_72/ddr4_c3.xdc
+    ../../../../xdc/vu13p_genbin.xdc
+    ../../../../xdc/vu13p_pcie.xdc
+}
+    #../../../../xdc/vu13p_clock.xdc
+    #../../../../xdc/vu13p_led.xdc
