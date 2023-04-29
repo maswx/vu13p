1c9d9a7b17ea88964b81470575660900b836930c
masw <masw@masw.tech>
2023-04-29 22:51:38 +0800

    add ltxname

--- download/downloadbit.tcl
+++ download/downloadbit.tcl
@@ -1,11 +1,12 @@
 set bitname [lindex $argv 0] 
+set ltxname [lindex $argv 1] 
 open_hw_manager
 connect_hw_server
 open_hw_target
 current_hw_device [get_hw_devices xcvu13p_0]
 refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xcvu13p_0] 0]
 #program_hw_devices -file $bitname 
-set_property PROBES.FILE {} [get_hw_devices xcvu13p_0]
+set_property PROBES.FILE $ltxname [get_hw_devices xcvu13p_0]
 set_property FULL_PROBES.FILE {} [get_hw_devices xcvu13p_0]
 set_property PROGRAM.FILE $bitname [get_hw_devices xcvu13p_0]
 program_hw_devices [get_hw_devices xcvu13p_0]
