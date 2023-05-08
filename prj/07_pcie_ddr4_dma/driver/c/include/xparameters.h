df943f1dac511703e2971b183e1f7e0b2fdba6d9
masw <masw@masw.tech>
2023-05-09 00:19:43 +0800

    update

---
+++ prj/07_pcie_ddr4_dma/driver/c/include/xparameters.h
@@ -0,0 +1,25 @@
+//========================================================================
+//        author   : masw
+//        email    : masw@masw.tech     
+//        creattime: 2023年05月08日 星期一 19时48分09秒
+//========================================================================
+
+#ifndef __XPARAMETERS_H__
+#define __XPARAMETERS_H__
+
+#define XPAR_XIIC_NUM_INSTANCES     1          /* Number of instances */
+
+#define XPAR_IIC_0_DEVICE_ID        90         /* Device ID for instance */
+#define XPAR_IIC_0_BASEADDR         0x00840000 /* Device base address */
+#define XPAR_IIC_0_TEN_BIT_ADR      FALSE      /* Supports 10 bit addresses */
+#define XPAR_IIC_0_GPO_WIDTH        1
+
+#define XPAR_IIC_1_DEVICE_ID        90         /* Device ID for instance */
+#define XPAR_IIC_1_BASEADDR         0x00840000 /* Device base address */
+#define XPAR_IIC_1_TEN_BIT_ADR      FALSE      /* Supports 10 bit addresses */
+#define XPAR_IIC_1_GPO_WIDTH        1
+
+#define __MICROBLAZE__
+
+
+#endif
