540bb574af92895644748fab705bb25eb36ebb56
masw <masw@masw.tech>
2023-08-16 23:17:52 +0800

    可以编译

--- prj/07_pcie_ddr4_dma/driver/c/main.c
+++ prj/07_pcie_ddr4_dma/driver/c/main.c
@@ -8,7 +8,7 @@
 #include "xparameters.h"
 #include "xiic_l.h"
 #include "xiic.h"
-#include "xintc.h"
+#include "xiic_wbwr.h"
 
 // FIR 的系数初值:	coeff_00 <= 16'd54    ;
 // FIR 的系数初值:	coeff_01 <= 16'd159   ;
@@ -48,55 +48,16 @@
 
 off_t base_offset = 0x00800000;  // 基地址的偏移量
 size_t mapping_size = 64 * 1024; // 映射的大小，64K
-int reset_fir_module(UINTPTR baseaddr);
-u32 wishbone_read(UINTPTR baseaddr, u32 addr);
-
-static int SetupInterrupSystem(XIic * iicinst)
-{
-
-
-
-}
-
-static int init_iic(XIic *iicinst, XIntc *iicintp, UINTPTR baseaddr_axiiic, UINTPTR baseaddr_interp)
-{
-	int status;
-	XIic_Config *ConfigPtr;	//包含 devid, baseaddress, has10bitAddr, GpOutWidth
-
-	//1. 初始化 ConfigPtr
-	ConfigPtr->DeviceId     = 0;
-	ConfigPtr->BaseAddress  = baseaddr_axiiic;
-	ConfigPtr->GpOutWidth   = 1;
-	ConfigPtr->Has10BitAddr = 0;
-
-	//2. 初始化 IIC 
-	status = XIic_CfgInitialize(iicinst, ConfigPtr, baseaddr_axiiic);
-	if(status != XST_SUCCESS)
-		return XST_FAILURE;
-	
-	//3. 初始化动态IIC 内核
-	status = XIic_DynamicInitialize(iicinst);
-	if(status != XST_SUCCESS)
-		return XST_FAILURE;
-
-	//4. 初始化中断系统
-	status = SetupInterrupSystem(iicinst);
-	if(status != XST_SUCCESS)
-		return XST_FAILURE;
-
-}
 
 
 
 int main(void) {
-    int axilte, interp;
+    int axilte;
 	int i;
     void *mapped_base;
 	UINTPTR base_address;
 	u8 rxdata[16];
 	u8 txdata[16];
-	XIic  iicinst;//iic模块
-	XIntc iicintp;//中断
 
 	for(i = 0; i < 16; i++)
 	{
@@ -106,11 +67,9 @@ int main(void) {
 
     // 打开设备文件
     axilte = open("/dev/xdma0_user", O_RDWR | O_SYNC);//axi lite设备
-    interp = open("/dev/xdma0_events_3", O_RDONLY);//IIC 专用中断设备
-    if (axilte == -1 || interp == -1) {
+    if (axilte == -1) {
         perror("Failed to open device");
 		close(axilte);
-    	close(interp);
         return -1;
     }
 
@@ -119,7 +78,6 @@ int main(void) {
     if (mapped_base == MAP_FAILED) {
         perror("Failed to map memory");
 		close(axilte);
-    	close(interp);
         return -1;
     }
 
@@ -130,9 +88,10 @@ int main(void) {
 
 
     // 使用 base_address 进行操作
-	u32 readbyte ;
-	readbyte = XIic_Recv(base_address, IIC_DEV_ADDR , rxdata, 2, XIIC_STOP);
-	//rxdata[0] = wishbone_read(base_address, 0);
+	unsigned readbyte ;
+	AddressType wishboneAddr = 0x00;
+	u16 ByteCount = 2;
+	readbyte = WishboneReadByte(base_address, IIC_DEV_ADDR , wishboneAddr, rxdata, ByteCount);
 
 
 	printf("readbyte=%x\n", readbyte);
@@ -141,7 +100,6 @@ int main(void) {
 
     // 解除内存映射并关闭文件
     munmap(mapped_base, mapping_size);
-    close(interp);
     close(axilte);
 
     return 0;
@@ -155,28 +113,4 @@ int reset_fir_module(UINTPTR baseaddr)
 	Xil_Out32(baseaddr + 0x00000124, 0x000000ff);
 	return 0;
 }
-
-u32 wishbone_read(UINTPTR baseaddr, u32 addr)
-{
-	u32 readbyte;
-	u8 readback[16];
-	u8 txdata[2];
-	txdata[0] = 0x00;
-	//readbyte = XIic_Recv(baseaddr, IIC_DEV_ADDR , readback, 2, XIIC_STOP);
-	//readbyte = XIic_Recv(baseaddr, IIC_DEV_ADDR , readback, 2, XIIC_REPEATED_START );
-	//XIic_Send(baseaddr, IIC_DEV_ADDR , txdata, 1, XIIC_STOP);
-	readbyte = XIic_Recv(baseaddr, IIC_DEV_ADDR , readback, 2, XIIC_STOP);
-	printf("readbyte count = %d\n", readbyte);
-	printf("readback[0] = %x\n", readback[0]);
-	printf("readback[1] = %x\n", readback[1]);
-
-	return 0;
-}
-
-u32 wishbone_write(UINTPTR baseaddr, u32 addr, u32 data)
-{
-
-	return 0;
-}
-
 
