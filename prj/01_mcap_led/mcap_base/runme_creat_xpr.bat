cafd8a3e2244f9cf4552e87f8403e9ede79c093f
masw <masw@masw.tech>
2023-11-04 16:41:58 +0800

    文档移动

--- prj/01_mcap_led/mcap_base/runme_creat_xpr.bat
+++
@@ -1,4 +0,0 @@
-:: script for Windows
-:: NOTES: add your vivado path to environment !
-vivado -mode batch -source project.tcl
-cp -rf ./project.srcs ./project/
--- prj/01_mcap_led/mcap_base/runme_creat_xpr.sh
+++
@@ -1,4 +0,0 @@
-# script for linux 
-# NOTES: add your vivado path to environment !
-vivado -mode batch -source project.tcl
-cp -rf ./project.srcs ./project/
