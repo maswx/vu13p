0a411a424922367f6542e50e4d37100fa604b669
masw <masw@masw.tech>
2023-11-05 01:10:21 +0800

    修复bug

--- prj/01_mcap_led/tcl/prj_genbit.tcl
+++ prj/01_mcap_led/tcl/prj_genbit.tcl
@@ -5,6 +5,13 @@ set current_time  [clock format [clock seconds] -format "%Y%m%d_%H%M"]
 
 open_project ${output_path}/${fpga_top_name}.xpr
 open_run impl_1
-write_bitstream -force fpga.runs/impl_1/${fpga_top_name}_${current_time}_${tag}.bit
-write_debug_probes -force fpga.runs/impl_1/${fpga_top_name}_${current_time}_${tag}.ltx
+
+write_bitstream    -force ${fpga_top_name}_${current_time}_${tag}.bit
+write_debug_probes -force ${fpga_top_name}_${current_time}_${tag}.ltx
+
+write_cfgmem -format bin -file "${fpga_top_name}_${current_time}_${tag}.bin" -size 256 -interface SPIx4 -loadbit \
+                {up 0x00000000 "${fpga_top_name}_${current_time}_${tag}_tandem1.bit" \
+                 up 0x02000000 "${fpga_top_name}_${current_time}_${tag}_tandem2.bit" } 
+			 # 前32M留给tandem1 
+
 
