5ae0ae1a1e6a15db24fee089c2fad6e71d9ca3de
masw <masw@masw.tech>
2023-11-05 15:19:14 +0800

    新增QPSK驱动

---
+++ prj/01_mcap_led/driver/qspi/compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#========================================================================
+#        author   : masw
+#        email    : masw@masw.tech     
+#        creattime: 2023年05月09日 星期二 00时22分51秒
+#========================================================================
+
+
+if [ ! -d build ]; then
+	mkdir build
+fi
+
+cd build
+cmake ../
+make 
+cd -
+cp ./build/xqspips_g128_flash_example .
