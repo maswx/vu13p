a3cea031c4e11bdefbdedf3076085dccee3a5461
masw <masw@masw.tech>
2023-11-12 13:25:05 +0800

    add qsfp cons

--- prj/05_pcie_ddr4_qsfp/tcl/add_constrs.tcl
+++ prj/05_pcie_ddr4_qsfp/tcl/add_constrs.tcl
@@ -2,5 +2,6 @@ add_files -fileset constrs_1 {
 	../../xdc/vu13p_clock.xdc
 	../../xdc/vu13p_led.xdc
 	../../xdc/vu13p_pcie.xdc
+	../../xdc/vu13p_qsfp.xdc
 	../../xdc/ddr4_64/ddr4_c0.xdc
 }             
