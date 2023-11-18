b5f1c45801658fe748a0f48e4d665920e1856154
masw <masw@masw.tech>
2023-11-18 23:20:59 +0800

    update

--- prj/02_pcie_ddr4x4/driver/ddr4test/src/main.c
+++ prj/02_pcie_ddr4x4/driver/ddr4test/src/main.c
@@ -1,165 +1,74 @@
-#include "stdio.h"
-#include <assert.h>
-#include <fcntl.h>
-#include <getopt.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <time.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <stdarg.h>
-#include <syslog.h>
 #include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/sysinfo.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <string.h>
-#define _LARGEFILE64_SOURCE     /* See feature_test_macros(7) */
-/* ltoh: little to host */
-/* htol: little to host */
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-#  define ltohl(x)       (x)
-#  define ltohs(x)       (x)
-#  define htoll(x)       (x)
-#  define htols(x)       (x)
-#elif __BYTE_ORDER == __BIG_ENDIAN
-#  define ltohl(x)     __bswap_32(x)
-#  define ltohs(x)     __bswap_16(x)
-#  define htoll(x)     __bswap_32(x)
-#  define htols(x)     __bswap_16(x)
-#endif
-#define __USE_FILE_OFFSET64
-#define __USE_LARGEFILE64
-#define _LARGEFILE64_SOURCE
+#include <sys/mman.h>
+#include <time.h>
+#include <stdint.h>
 
+#define DEVICE_READ "/dev/xdma0_c2h_0"
+#define DEVICE_WRITE "/dev/xdma0_h2c_0"
+#define SIZE 0x3FFFFFFFF
 
-#define MAP_SIZE (1024*1024UL)
-#define MAP_MASK (MAP_SIZE - 1)
- 
-#define FPGA_AXI_START_ADDR (0)
- 
-void *control_base;
-int control_fd;
-int c2h_dma_fd;
-int h2c_dma_fd;
- 
-static unsigned long h2c_fpga_ddr_addr;
-static unsigned long c2h_fpga_ddr_addr;
- 
- 
-static int open_control(char *filename)
-{
-    int fd;
-    fd = open(filename, O_RDWR | O_SYNC);
-    if(fd == -1)
-    {
-        printf("open control error\n");
-        return -1;
+int main() {
+    int fd_read = open(DEVICE_READ, O_RDWR | O_SYNC);
+    if (fd_read < 0) {
+        perror("Failed to open the read device");
+        return EXIT_FAILURE;
     }
-    return fd;
-}
-static void *mmap_control(int fd,long mapsize)
-{
-    void *vir_addr;
-    vir_addr = mmap(0, mapsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    return vir_addr;
-}
-void write_control(int offset,uint32_t val)
-{
-    uint32_t writeval = htoll(val);
-    *((uint32_t *)(control_base+offset)) = writeval;
-}
-uint32_t read_control(int offset)
-{
-    uint32_t read_result = *((uint32_t *)(control_base+offset));
-    read_result = ltohl(read_result);
-    return read_result;
-}
- 
- 
-void put_data_to_fpga_ddr(unsigned long fpga_ddr_addr,short int *buffer,unsigned int len)
-{
-    if(h2c_dma_fd >= 0)
-    {
-		printf("write data to fpga\n");
-        lseek(h2c_dma_fd,fpga_ddr_addr,SEEK_SET);
-        write(h2c_dma_fd,buffer,len*2);
+
+    int fd_write = open(DEVICE_WRITE, O_RDWR | O_SYNC);
+    if (fd_write < 0) {
+        close(fd_read);
+        perror("Failed to open the write device");
+        return EXIT_FAILURE;
     }
-}
-void get_data_from_fpga_ddr(unsigned long fpga_ddr_addr,short int  *buffer,unsigned int len)
-{
-    if(c2h_dma_fd >= 0)
-    {
-		printf("read data from fpga\n");
-        lseek(c2h_dma_fd,fpga_ddr_addr,SEEK_SET);
-        read(c2h_dma_fd,buffer,len*2);
+
+    uintptr_t *map_base_read = mmap(0, SIZE, PROT_READ, MAP_SHARED, fd_read, 0);
+    if (map_base_read == MAP_FAILED) {
+        close(fd_read);
+        close(fd_write);
+        perror("Failed to map the read device");
+        return EXIT_FAILURE;
     }
+
+    uintptr_t *map_base_write = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, fd_write, 0);
+    if (map_base_write == MAP_FAILED) {
+        munmap(map_base_read, SIZE);
+        close(fd_read);
+        close(fd_write);
+        perror("Failed to map the write device");
+        return EXIT_FAILURE;
+    }
+
+    // Test the memory.
+    time_t start_time = time(NULL);
+    for (unsigned long i = 0; i < SIZE; i += 4) {
+        *((uint64_t *)(map_base_write + i)) = i;
+    }
+    time_t end_time = time(NULL);
+    double timxx = (double)SIZE / (end_time - start_time);
+    printf("写入16GB内存的速度为: %f MB/s\n", timxx);
+
+    start_time = time(NULL);
+    for (unsigned long i = 0; i < SIZE; i += 4) {
+        if (*((uint64_t *)(map_base_read + i)) != i) {
+            printf("Memory test failed at offset 0x%lx\n", i);
+            break;
+        }
+    }
+    end_time = time(NULL);
+    printf("读出并比对16GB内存的时间为: %f s\n",  (end_time - start_time));
+
+    if (munmap(map_base_read, SIZE) == -1 || munmap(map_base_write, SIZE) == -1) {
+        close(fd_read);
+        close(fd_write);
+        perror("Failed to unmap the devices");
+        return EXIT_FAILURE;
+    }
+
+    close(fd_read);
+    close(fd_write);
+    return EXIT_SUCCESS;
 }
- 
-int pcie_init()
-{
-    c2h_dma_fd = open("/dev/xdma0_c2h_0",O_RDONLY);
-    if(c2h_dma_fd < 0)
-	{
-		printf("error in c2h_dma_fd %x\n", c2h_dma_fd);
-	}
-    h2c_dma_fd = open("/dev/xdma0_h2c_0",O_WRONLY);
-    //h2c_dma_fd = open("/dev/xdma0_h2c_0",O_RDONLY);
-    if(h2c_dma_fd < 0)
-	{
-		printf("error in h2c_dma_fd\n");
-	}
-    control_fd = open_control("/dev/xdma0_control");
-    if(control_fd < 0)
-	{
-		printf("error in ctrl\n");
-	}
-    control_base = mmap_control(control_fd,MAP_SIZE);
-    return 1;
-}
-void pcie_deinit()
-{
-    close(c2h_dma_fd);
-    close(h2c_dma_fd);
-    close(control_fd);
-}
-
-int main(void){
-	int i=0;
-    int len=0x10010;
-	short int *bufferin = (short int *)malloc(len*(sizeof(short int)));
-	short int *bufferot = (short int *)malloc(len*(sizeof(short int)));
-	for(i = 0; i < len;i++)
-	{
-		bufferin[i] = i;
-		bufferot[i] = 0;
-	}
-    h2c_fpga_ddr_addr = 0x00000000;
-    c2h_fpga_ddr_addr = 0x00000000;
-	printf("fpga_ddr_addr = %lx\n", h2c_fpga_ddr_addr);
-    printf("c2h_dma_fd = %x\n", c2h_dma_fd);
-    printf("h2c_dma_fd = %x\n", h2c_dma_fd);
-    printf("control_fd = %x\n", control_fd);
-	pcie_init();
-
-	put_data_to_fpga_ddr  (h2c_fpga_ddr_addr,bufferin,len);
-	get_data_from_fpga_ddr(c2h_fpga_ddr_addr,bufferot,len);
-
-	for(i = 0; i < len;i++)
-		printf("bufferin[%02d]=%d, bufferot[%02d]=%d\n",i,bufferin[i], i,bufferot[i]);
-
-	pcie_deinit();
-	free(bufferin);
-	free(bufferot);
-	return 0;
-}
-
-
-
-
