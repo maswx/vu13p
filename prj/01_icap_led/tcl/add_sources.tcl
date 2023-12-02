8ca2d737afc0d9175cd8993286f9a921cea59311
masw <masw@masw.tech>
2023-12-03 01:42:45 +0800

    update

--- prj/01_icap_led/tcl/add_sources.tcl
+++ prj/01_icap_led/tcl/add_sources.tcl
@@ -3,13 +3,9 @@ set_property file_type "Verilog Header" [get_files ./src/define.v]
 set_property is_global_include true     [get_files ./src/define.v]
 
 add_files -fileset sources_1 {
-	./src/fpga_reboot.v 
-	./src/mcap_led_top.v
-	./src/tandem_app_bram.v
-	./src/tandem_app_axixvc.v
-	./src/tandem_app_led.v
-	./src/xdma_mcap_qspi.v
-	./src/axil_interconnect_wrap_1x5.v
+	./src/example1_led_top.v
+	./src/example2_bram_top.v
+	./src/golden_image_top.v
 }
 
 add_files "
