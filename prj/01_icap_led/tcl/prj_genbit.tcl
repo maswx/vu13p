04fa553147d6d53fff76370efd8f6e6144c9631c
masw <masw@masw.tech>
2023-12-09 00:09:49 +0800

    不用multiboot了，直接将bin存到两个区中好了

--- prj/01_icap_led/tcl/prj_genbit.tcl
+++ prj/01_icap_led/tcl/prj_genbit.tcl
@@ -6,10 +6,11 @@ set current_time  [clock format [clock seconds] -format "%Y%m%d_%H%M"]
 open_project ${output_path}/${fpga_top_name}.xpr
 open_run impl_1
 
-set_property BITSTREAM.CONFIG.CONFIGFALLBACK   ENABLE     [current_design] ;#使能回退
-set_property BITSTREAM.GENERAL.COMPRESS         TRUE       [current_design]
-set_property BITSTREAM.CONFIG.SPI_BUSWIDTH     4          [current_design]
-set_property BITSTREAM.CONFIG.NEXT_CONFIG_ADDR 0x08000000 [current_design] ;# update inmage 不能带这句话，仅仅Golden Image可以带
+set_property BITSTREAM.CONFIG.CONFIGFALLBACK     ENABLE     [current_design] ;#使能回退
+set_property BITSTREAM.GENERAL.COMPRESS          TRUE       [current_design]
+set_property BITSTREAM.CONFIG.SPI_BUSWIDTH       4          [current_design]
+#set_property BITSTREAM.CONFIG.NEXT_CONFIG_ADDR   0x08000000 [current_design] ;# update inmage 不能带这句话，仅仅Golden Image可以带
+#set_property BITSTREAM.CONFIG.NEXT_CONFIG_REBOOT ENABLE     [current_design] ;# update inmage 不能带这句话，仅仅Golden Image可以带
 
 write_bitstream    -force ${fpga_top_name}_${current_time}_${tag}.bit
 write_debug_probes -force ${fpga_top_name}_${current_time}_${tag}.ltx
