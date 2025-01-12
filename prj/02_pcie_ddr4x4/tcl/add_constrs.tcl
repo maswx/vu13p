4c704623907f8571d10a0ad3b183462b16a0c354
masw <masw@masw.tech>
2025-01-12 14:17:16 +0800

    更新了makefile

--- prj/02_pcie_ddr4x4/tcl/add_constrs.tcl
+++
@@ -1,7 +0,0 @@
-add_files -fileset constrs_1 {
-	../../xdc/vu13p_pcie.xdc
-	../../xdc/ddr4_64/ddr4_c0.xdc
-	../../xdc/ddr4_64/ddr4_c1.xdc
-	../../xdc/ddr4_64/ddr4_c2.xdc
-	../../xdc/ddr4_64/ddr4_c3.xdc
-}             
--- prj/02_pcie_ddr4x4/tcl/add_sources.tcl
+++
@@ -1,5 +0,0 @@
-
-add_files -fileset sources_1 {
-	./src/xdma_ddr4x4.bd
-	./src/xdma_ddr4x4_top.v
-}
