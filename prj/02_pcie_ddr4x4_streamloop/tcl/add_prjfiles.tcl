1aa91aa56b5e04e0ba0b46ddfacd4b57c2f19dcd
masw <masw@masw.tech>
2025-01-15 20:01:43 +0800

    增加DMA设计

---
+++ prj/02_pcie_ddr4x4_streamloop/tcl/add_prjfiles.tcl
@@ -0,0 +1,25 @@
+
+
+
+
+
+read_verilog {
+
+	./src/pcie_ddr4x4_streamloop.v
+}
+
+
+
+
+read_xdc {
+    ../../xdc/vu13p_pcie.xdc
+    ../../xdc/multiboot.xdc
+    ../../xdc/ddr4_64/ddr4_c0.xdc
+    ../../xdc/ddr4_64/ddr4_c1.xdc
+    ../../xdc/ddr4_64/ddr4_c2.xdc
+    ../../xdc/ddr4_64/pblock_ddr4_c0.xdc
+    ../../xdc/ddr4_64/pblock_ddr4_c1.xdc
+    ../../xdc/ddr4_64/pblock_ddr4_c2.xdc
+	./tcl/tandem_pblock_placement.xdc
+}
+
