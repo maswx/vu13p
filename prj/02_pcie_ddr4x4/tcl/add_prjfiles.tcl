19ba53f14d7b8005ed4371f6f39bff91299d3b0b
masw <masw@masw.tech>
2025-01-16 14:46:38 +0800

    using bitsream.xdc

--- prj/02_pcie_ddr4x4/tcl/add_prjfiles.tcl
+++ prj/02_pcie_ddr4x4/tcl/add_prjfiles.tcl
@@ -7,7 +7,7 @@ read_verilog {
 
 read_xdc {
 	../../xdc/vu13p_pcie.xdc
-	../../xdc/multiboot.xdc
+	../../xdc/bitstream.xdc
 	../../xdc/ddr4_64/ddr4_c0.xdc
 	../../xdc/ddr4_64/ddr4_c1.xdc
 	../../xdc/ddr4_64/ddr4_c2.xdc
