d6af334e6974c61097e60c1fbebca6e052eaebec
masw <masw@masw.tech>
2025-03-11 19:27:41 +0800

    rm 01 icap led

--- prj/01_icap_led/tcl/ip_axil_bram.tcl
+++
@@ -1,7 +0,0 @@
-create_ip -name axi_bram_ctrl -vendor xilinx.com -library ip  -module_name axi_bram_ctrl_1
-set_property -dict [list \
-  CONFIG.BMG_INSTANCE {INTERNAL} \
-  CONFIG.MEM_DEPTH {1024} \
-  CONFIG.PROTOCOL {AXI4LITE} \
-  CONFIG.SINGLE_PORT_BRAM {1} \
-] [get_ips axi_bram_ctrl_1]
--- prj/01_icap_led/tcl/ip_ila_axil32.tcl
+++
@@ -1,7 +0,0 @@
-create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name ila_axil32 
-set_property -dict [list \
-  CONFIG.C_DATA_DEPTH {2048} \
-  CONFIG.C_MONITOR_TYPE {AXI} \
-  CONFIG.C_SLOT_0_AXI_PROTOCOL {AXI4LITE} \
-  CONFIG.Component_Name {ila_axil32} \
-] [get_ips ila_axil32]
